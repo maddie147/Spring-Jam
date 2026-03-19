d7b5a12e91e7e8a3b504149167569e623ec65a06
WorkingBean <jfuentes9626@gmail.com>
2026-03-18 22:37:40 -0400

    background added

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var sprite = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +12,6 @@ func _ready() -> void:
 
 func _toggle_flower():
 	sprite.frame = 1
+	#animated_sprite_2d.play("default")
+	#animated_sprite_2d.stop()
+	
